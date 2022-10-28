b817a56cfddb5a3a8a718158badd8cedd43f3a6e
lucas <beishengjiang@gmail.com>
2022-10-28 22:30:15 +0800

    单例简单注释

--- src/main/java/headfirst/designpatterns/singleton/classic/Singleton.java
+++ src/main/java/headfirst/designpatterns/singleton/classic/Singleton.java
@@ -2,20 +2,44 @@ package headfirst.designpatterns.singleton.classic;
 
 // NOTE: This is not thread safe!
 
+/**
+ * 获取单例对象类
+ *
+ * @author lucas
+ * @since 2022年10月28日22:21:43
+ */
 public class Singleton {
-	private static Singleton uniqueInstance;
- 
-	private Singleton() {}
- 
-	public static Singleton getInstance() {
-		if (uniqueInstance == null) {
-			uniqueInstance = new Singleton();
-		}
-		return uniqueInstance;
-	}
- 
-	// other useful methods here
-	public String getDescription() {
-		return "I'm a classic Singleton!";
-	}
+    /**
+     * 私有静态成员单例对象
+     */
+    private static Singleton uniqueInstance;
+
+    /**
+     * 私有化构造防止创建对象
+     */
+    private Singleton() {
+    }
+
+    /**
+     * 公开静态方法获取单例对象
+     *
+     * @return 单例对象
+     */
+    public static Singleton getInstance() {
+        if (uniqueInstance == null) {
+            uniqueInstance = new Singleton();
+        }
+        return uniqueInstance;
+    }
+
+    // other useful methods here
+
+    /**
+     * 获取它的某个方法
+     *
+     * @return 无聊的字符串
+     */
+    public String getDescription() {
+        return "I'm a classic Singleton!";
+    }
 }
--- src/main/java/headfirst/designpatterns/singleton/classic/SingletonClient.java
+++ src/main/java/headfirst/designpatterns/singleton/classic/SingletonClient.java
@@ -1,8 +1,14 @@
 package headfirst.designpatterns.singleton.classic;
 
+/**
+ * 经典单例模式启动类
+ *
+ * @author lucas
+ * @since 2022年10月28日22:20:01
+ */
 public class SingletonClient {
-	public static void main(String[] args) {
-		Singleton singleton = Singleton.getInstance();
-		System.out.println(singleton.getDescription());
-	}
+    public static void main(String[] args) {
+        Singleton singleton = Singleton.getInstance();
+        System.out.println(singleton.getDescription());
+    }
 }
