8bc05a3035ad623f136b8f824ff0a414ee26c2a0
lucas <beishengjiang@gmail.com>
2022-11-09 08:28:55 +0800

    巧克力锅炉注释

--- src/main/java/headfirst/designpatterns/singleton/chocolate/ChocolateBoiler.java
+++ src/main/java/headfirst/designpatterns/singleton/chocolate/ChocolateBoiler.java
@@ -1,51 +1,94 @@
 package headfirst.designpatterns.singleton.chocolate;
- 
-public class ChocolateBoiler {
-	private boolean empty;
-	private boolean boiled;
-	private static ChocolateBoiler uniqueInstance;
-  
-	private ChocolateBoiler() {
-		empty = true;
-		boiled = false;
-	}
-  
-	public static ChocolateBoiler getInstance() {
-		if (uniqueInstance == null) {
-			System.out.println("Creating unique instance of Chocolate Boiler");
-			uniqueInstance = new ChocolateBoiler();
-		}
-		System.out.println("Returning instance of Chocolate Boiler");
-		return uniqueInstance;
-	}
 
-	public void fill() {
-		if (isEmpty()) {
-			empty = false;
-			boiled = false;
-			// fill the boiler with a milk/chocolate mixture
-		}
-	}
- 
-	public void drain() {
-		if (!isEmpty() && isBoiled()) {
-			// drain the boiled milk and chocolate
-			empty = true;
-		}
-	}
- 
-	public void boil() {
-		if (!isEmpty() && !isBoiled()) {
-			// bring the contents to a boil
-			boiled = true;
-		}
-	}
-  
-	public boolean isEmpty() {
-		return empty;
-	}
- 
-	public boolean isBoiled() {
-		return boiled;
-	}
+/**
+ * 巧克力锅炉
+ *
+ * @author lucas
+ * @since 2022年11月08日20:42:51
+ */
+public class ChocolateBoiler {
+    /**
+     * 单例模式经典声明静态类名牌变量
+     */
+    private static ChocolateBoiler uniqueInstance;
+    /**
+     * 空
+     */
+    private boolean empty;
+    /**
+     * 沸腾
+     */
+    private boolean boiled;
+
+    /**
+     * 私有无参构造
+     */
+    private ChocolateBoiler() {
+        empty = true;
+        boiled = false;
+    }
+
+    /**
+     * 单例模式获取对象的典型静态构造方法
+     * 经典是因为都叫getInstance
+     *
+     * @return 巧克力锅炉
+     */
+    public static ChocolateBoiler getInstance() {
+        if (uniqueInstance == null) {
+            System.out.println("Creating unique instance of Chocolate Boiler");
+            uniqueInstance = new ChocolateBoiler();
+        }
+        System.out.println("Returning instance of Chocolate Boiler");
+        return uniqueInstance;
+    }
+
+    /**
+     * 填满
+     */
+    public void fill() {
+        if (isEmpty()) {
+            empty = false;
+            boiled = false;
+            // fill the boiler with a milk/chocolate mixture
+        }
+    }
+
+    /**
+     * 滤干煮过的牛奶和巧克力
+     */
+    public void drain() {
+        if (!isEmpty() && isBoiled()) {
+            // drain the boiled milk and chocolate
+            empty = true;
+        }
+    }
+
+    /**
+     * 把里面的东西煮开
+     */
+    public void boil() {
+        if (!isEmpty() && !isBoiled()) {
+            // bring the contents to a boil
+            boiled = true;
+        }
+    }
+
+    /**
+     * 是否为空
+     *
+     * @return 布尔值
+     */
+    public boolean isEmpty() {
+        return empty;
+    }
+
+    /**
+     * 是否沸腾
+     *
+     * @return 是否
+     */
+    public boolean isBoiled() {
+        return boiled;
+    }
 }
--- src/main/java/headfirst/designpatterns/singleton/chocolate/ChocolateController.java
+++ src/main/java/headfirst/designpatterns/singleton/chocolate/ChocolateController.java
@@ -1,13 +1,19 @@
 package headfirst.designpatterns.singleton.chocolate;
- 
-public class ChocolateController {
-	public static void main(String args[]) {
-		ChocolateBoiler boiler = ChocolateBoiler.getInstance();
-		boiler.fill();
-		boiler.boil();
-		boiler.drain();
 
-		// will return the existing instance
-		ChocolateBoiler boiler2 = ChocolateBoiler.getInstance();
-	}
+/**
+ * 巧克力控制器
+ *
+ * @author lucas
+ * @since 2022年11月08日20:48:31
+ */
+public class ChocolateController {
+    public static void main(String[] args) {
+        ChocolateBoiler boiler = ChocolateBoiler.getInstance();
+        boiler.fill();
+        boiler.boil();
+        boiler.drain();
+
+        // will return the existing instance
+        ChocolateBoiler boiler2 = ChocolateBoiler.getInstance();
+    }
 }
