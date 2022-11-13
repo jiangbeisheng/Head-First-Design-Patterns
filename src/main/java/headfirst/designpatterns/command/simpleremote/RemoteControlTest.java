dc82c4d31535f6f242762008afbd5535ba10cdeb
lucas <beishengjiang@gmail.com>
2022-11-13 17:28:44 +0800

    简单 命令模式 添加注释

--- src/main/java/headfirst/designpatterns/command/simpleremote/RemoteControlTest.java
+++ src/main/java/headfirst/designpatterns/command/simpleremote/RemoteControlTest.java
@@ -9,24 +9,30 @@ package headfirst.designpatterns.command.simpleremote;
 public class RemoteControlTest {
     public static void main(String[] args) {
         //1.创建控制器对象
-        SimpleRemoteControl remote = new SimpleRemoteControl();
+        SimpleRemoteControl control = new SimpleRemoteControl();
         //2.创建灯对象
         Light light = new Light();
         //3.创建车库门对象
         GarageDoor garageDoor = new GarageDoor();
         //4.创建开灯命令
-        LightOnCommand lightOn = new LightOnCommand(light);
+        LightOnCommand lightOnCommand = new LightOnCommand(light);
+        LightOffCommand lightOffCommand = new LightOffCommand(light);
         //5.创建车库门开关
-        GarageDoorOpenCommand garageOpen = new GarageDoorOpenCommand(garageDoor);
+        GarageDoorOpenCommand garageDoorOpenCommand = new GarageDoorOpenCommand(garageDoor);
 
         //控制器设置开灯命令
-        remote.setCommand(lightOn);
+        control.setCommand(lightOnCommand);
         //控制器按钮已按下
-        remote.buttonWasPressed();
+        control.buttonWasPressed();
+
+        //the same to above
+        control.setCommand(lightOffCommand);
+        control.buttonWasPressed();
+
         //控制器设置命令
-        remote.setCommand(garageOpen);
+        control.setCommand(garageDoorOpenCommand);
         //控制器按钮被按下
-        remote.buttonWasPressed();
+        control.buttonWasPressed();
     }
 
 }
