83f20d26dd357ea56f3fc6fb9d0b03ee09664d59
lucas <beishengjiang@gmail.com>
2022-12-07 22:32:03 +0800

    迭代器模式 一三五 二四六

--- src/main/java/headfirst/designpatterns/iterator/dinermerger/AlternatingDinerMenuIterator.java
+++ src/main/java/headfirst/designpatterns/iterator/dinermerger/AlternatingDinerMenuIterator.java
@@ -2,27 +2,32 @@ package headfirst.designpatterns.iterator.dinermerger;
 
 import java.util.Calendar;
 
+/**
+ * 交替食客菜单迭代器
+ *
+ * @author lucas
+ * @since 2022年12月07日22:20:22
+ */
 public class AlternatingDinerMenuIterator implements Iterator {
-	MenuItem[] list;
-	int position;
+    MenuItem[] list;
+    int position;
 
-	public AlternatingDinerMenuIterator(MenuItem[] list) {
-		this.list = list;
-		position = Calendar.DAY_OF_WEEK % 2;
-	}
-	public MenuItem next() {
-		MenuItem menuItem = list[position];
-		position = position + 2;
-		return menuItem;
-	}
-	public boolean hasNext() {
-		if (position >= list.length || list[position] == null) {
-			return false;
-		} else {
-			return true;
-		}
-	}
-	public String toString() {
-		return "Alternating Diner Menu Iterator";
-	}
+    public AlternatingDinerMenuIterator(MenuItem[] list) {
+        this.list = list;
+        position = Calendar.DAY_OF_WEEK % 2;
+    }
+
+    public MenuItem next() {
+        MenuItem menuItem = list[position];
+        position = position + 2;
+        return menuItem;
+    }
+
+    public boolean hasNext() {
+        return position < list.length && list[position] != null;
+    }
+
+    public String toString() {
+        return "Alternating Diner Menu Iterator";
+    }
 }
