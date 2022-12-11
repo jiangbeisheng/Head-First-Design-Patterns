1af0c6073bc76da82a70d6592a99d31a0942bc71
lucas <beishengjiang@gmail.com>
2022-12-11 22:32:45 +0800

    迭代器模式 组合模式 添加注释

--- src/main/java/headfirst/designpatterns/composite/menuiterator/Menu.java
+++ src/main/java/headfirst/designpatterns/composite/menuiterator/Menu.java
@@ -1,57 +1,63 @@
 package headfirst.designpatterns.composite.menuiterator;
 
-import java.util.Iterator;
 import java.util.ArrayList;
+import java.util.Iterator;
 
+/**
+ * Base菜单-大菜单
+ *
+ * @author lucas
+ * @since 2022年12月09日10:09:23
+ */
 public class Menu extends MenuComponent {
-	Iterator<MenuComponent> iterator = null;
-	ArrayList<MenuComponent> menuComponents = new ArrayList<MenuComponent>();
-	String name;
-	String description;
-  
-	public Menu(String name, String description) {
-		this.name = name;
-		this.description = description;
-	}
- 
-	public void add(MenuComponent menuComponent) {
-		menuComponents.add(menuComponent);
-	}
- 
-	public void remove(MenuComponent menuComponent) {
-		menuComponents.remove(menuComponent);
-	}
- 
-	public MenuComponent getChild(int i) {
-		return menuComponents.get(i);
-	}
- 
-	public String getName() {
-		return name;
-	}
- 
-	public String getDescription() {
-		return description;
-	}
+    Iterator<MenuComponent> iterator = null;
+    ArrayList<MenuComponent> menuComponents = new ArrayList<>();
+    String name;
+    String description;
 
-  
-	public Iterator<MenuComponent> createIterator() {
-		if (iterator == null) {
-			iterator = new CompositeIterator(menuComponents.iterator());
-		}
-		return iterator;
-	}
- 
- 
-	public void print() {
-		System.out.print("\n" + getName());
-		System.out.println(", " + getDescription());
-		System.out.println("---------------------");
-  
-		Iterator<MenuComponent> iterator = menuComponents.iterator();
-		while (iterator.hasNext()) {
-			MenuComponent menuComponent = iterator.next();
-			menuComponent.print();
-		}
-	}
+    public Menu(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public void add(MenuComponent menuComponent) {
+        menuComponents.add(menuComponent);
+    }
+
+    public void remove(MenuComponent menuComponent) {
+        menuComponents.remove(menuComponent);
+    }
+
+    public MenuComponent getChild(int i) {
+        return menuComponents.get(i);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public Iterator<MenuComponent> createIterator() {
+        if (iterator == null) {
+            iterator = new CompositeIterator(menuComponents.iterator());
+        }
+        return iterator;
+    }
+
+
+    public void print() {
+        System.out.print("\n" + getName());
+        System.out.println(", " + getDescription());
+        System.out.println("---------------------");
+
+        Iterator<MenuComponent> iterator = menuComponents.iterator();
+        while (iterator.hasNext()) {
+            MenuComponent menuComponent = iterator.next();
+            menuComponent.print();
+        }
+    }
 }
--- src/main/java/headfirst/designpatterns/composite/menuiterator/MenuComponent.java
+++ src/main/java/headfirst/designpatterns/composite/menuiterator/MenuComponent.java
@@ -1,35 +1,52 @@
 package headfirst.designpatterns.composite.menuiterator;
 
-import java.util.*;
+import java.util.Iterator;
 
+/**
+ * 菜单组件-抽象类
+ *
+ * @author lucas
+ * @since 2022年12月09日10:08:11
+ */
 public abstract class MenuComponent {
-   
-	public void add(MenuComponent menuComponent) {
-		throw new UnsupportedOperationException();
-	}
-	public void remove(MenuComponent menuComponent) {
-		throw new UnsupportedOperationException();
-	}
-	public MenuComponent getChild(int i) {
-		throw new UnsupportedOperationException();
-	}
-  
-	public String getName() {
-		throw new UnsupportedOperationException();
-	}
-	public String getDescription() {
-		throw new UnsupportedOperationException();
-	}
-	public double getPrice() {
-		throw new UnsupportedOperationException();
-	}
-	public boolean isVegetarian() {
-		throw new UnsupportedOperationException();
-	}
 
-	public abstract Iterator<MenuComponent> createIterator();
- 
-	public void print() {
-		throw new UnsupportedOperationException();
-	}
+    public void add(MenuComponent menuComponent) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void remove(MenuComponent menuComponent) {
+        throw new UnsupportedOperationException();
+    }
+
+    public MenuComponent getChild(int i) {
+        throw new UnsupportedOperationException();
+    }
+
+    public String getName() {
+        throw new UnsupportedOperationException();
+    }
+
+    public String getDescription() {
+        throw new UnsupportedOperationException();
+    }
+
+    public double getPrice() {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean isVegetarian() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * 创建迭代器的方法
+     * 子类必须实现
+     *
+     * @return 迭代器
+     */
+    public abstract Iterator<MenuComponent> createIterator();
+
+    public void print() {
+        throw new UnsupportedOperationException();
+    }
 }
