f1d4c2148c79c4c0ea41d8a77aa458faa05976dd
lucas <beishengjiang@gmail.com>
2022-12-15 16:16:48 +0800

    迭代器模式 组合模式 透明性

--- src/main/java/headfirst/designpatterns/composite/menuiterator/CompositeIterator.java
+++ src/main/java/headfirst/designpatterns/composite/menuiterator/CompositeIterator.java
@@ -1,38 +1,60 @@
 package headfirst.designpatterns.composite.menuiterator;
 
-import java.util.*;
-  
+import java.util.Iterator;
+import java.util.Stack;
+
+/**
+ * 复合迭代器
+ *
+ * @author lucas
+ * @since 2022年12月15日10:51:42
+ */
 public class CompositeIterator implements Iterator<MenuComponent> {
-	Stack<Iterator<MenuComponent>> stack = new Stack<Iterator<MenuComponent>>();
-   
-	public CompositeIterator(Iterator<MenuComponent> iterator) {
-		stack.push(iterator);
-	}
-   
-	public MenuComponent next() {
-		if (hasNext()) {
-			Iterator<MenuComponent> iterator = stack.peek();
-			MenuComponent component = iterator.next();
-			stack.push(component.createIterator());
-			return component;
-		} else {
-			return null;
-		}
-	}
-  
-	public boolean hasNext() {
-		if (stack.empty()) {
-			return false;
-		} else {
-			Iterator<MenuComponent> iterator = stack.peek();
-			if (!iterator.hasNext()) {
-				stack.pop();
-				return hasNext();
-			} else {
-				return true;
-			}
-		}
-	}
+    Stack<Iterator<MenuComponent>> stack = new Stack<>();
+
+    /**
+     * 构造方法
+     *
+     * @param iterator 迭代器
+     */
+    public CompositeIterator(Iterator<MenuComponent> iterator) {
+        stack.push(iterator);
+    }
+
+    /**
+     * 下一个菜单组件
+     *
+     * @return 菜单组件
+     */
+    public MenuComponent next() {
+        if (hasNext()) {
+            Iterator<MenuComponent> iterator = stack.peek();
+            MenuComponent component = iterator.next();
+            stack.push(component.createIterator());
+            return component;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * 是否有下一个
+     *
+     * @return true 或 false
+     */
+    public boolean hasNext() {
+        if (stack.empty()) {
+            return false;
+        } else {
+            Iterator<MenuComponent> iterator = stack.peek();
+            if (!iterator.hasNext()) {
+                stack.pop();
+                return hasNext();
+            } else {
+                return true;
+            }
+        }
+    }
 	
 	/*
 	 * No longer needed as of Java 8
--- src/main/java/headfirst/designpatterns/composite/menuiterator/Waitress.java
+++ src/main/java/headfirst/designpatterns/composite/menuiterator/Waitress.java
@@ -1,29 +1,50 @@
 package headfirst.designpatterns.composite.menuiterator;
 
 import java.util.Iterator;
-  
-public class Waitress {
-	MenuComponent allMenus;
- 
-	public Waitress(MenuComponent allMenus) {
-		this.allMenus = allMenus;
-	}
- 
-	public void printMenu() {
-		allMenus.print();
-	}
-  
-	public void printVegetarianMenu() {
-		Iterator<MenuComponent> iterator = allMenus.createIterator();
 
-		System.out.println("\nVEGETARIAN MENU\n----");
-		while (iterator.hasNext()) {
-			MenuComponent menuComponent = iterator.next();
-			try {
-				if (menuComponent.isVegetarian()) {
-					menuComponent.print();
-				}
-			} catch (UnsupportedOperationException e) {}
-		}
-	}
+/**
+ * 女服务员
+ *
+ * @author lucas
+ * @since 2022年12月11日22:30:42
+ */
+public class Waitress {
+    /**
+     * 菜单组件
+     */
+    MenuComponent allMenus;
+
+    /**
+     * 有参构造
+     *
+     * @param allMenus 所有菜单
+     */
+    public Waitress(MenuComponent allMenus) {
+        this.allMenus = allMenus;
+    }
+
+    /**
+     * 打印菜单
+     */
+    public void printMenu() {
+        allMenus.print();
+    }
+
+    /**
+     * 打印素食菜单
+     */
+    public void printVegetarianMenu() {
+        Iterator<MenuComponent> iterator = allMenus.createIterator();
+
+        System.out.println("\nVEGETARIAN MENU\n----");
+        while (iterator.hasNext()) {
+            MenuComponent menuComponent = iterator.next();
+            try {
+                if (menuComponent.isVegetarian()) {
+                    menuComponent.print();
+                }
+            } catch (UnsupportedOperationException e) {
+            }
+        }
+    }
 }
